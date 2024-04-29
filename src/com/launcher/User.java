cd9a34396bd69f8e2630d87bbbb88cdbfd33f377
Anx0Fdez <anxofernandezrodriguez05@gmail.com>
2024-04-29 14:19:38 +0200

    creando array list

--- src/com/launcher/User.java
+++ src/com/launcher/User.java
@@ -1,4 +1,14 @@
 package com.launcher;
 
+import com.games.Game;
+
+import java.util.ArrayList;
+
 public class User {
+    private int userId;
+    private String userName;
+    private String password;
+    private String email;
+
+    ArrayList<Game> gamesOwned = new ArrayList<Game>();
 }
