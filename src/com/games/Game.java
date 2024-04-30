26b3b7775bc0e9c4ad5977c856cb2e946e948abe
Anx0Fdez <anxofernandezrodriguez05@gmail.com>
2024-04-30 10:50:41 +0200

    atributos clase GAME

--- src/com/games/Game.java
+++ src/com/games/Game.java
@@ -1,4 +1,13 @@
 package com.games;
 
 public class Game {
+    private int gameId;
+    private String gameName;
+    private String gameDescription;
+    private String gameGenre;
+    private String developer;
+    private String publisher;
+    private String price;
+    private double rating;
+
 }
