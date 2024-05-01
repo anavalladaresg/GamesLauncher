f8ff4a15ad10f20c4185188f69f02238d656cd5a
Ana Valladares González <avalladaresgonzalez@danielcastelao.org>
2024-05-01 11:49:09 +0200

    Implementada parte de registro

--- src/com/launcher/UIController.java
+++ src/com/launcher/UIController.java
@@ -3,6 +3,7 @@ package com.launcher;
 import com.games.Game;
 
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -36,43 +37,89 @@ public class UIController {
      * @param panel El panel en el que se colocarán los componentes.
      */
     private void placeComponents(JPanel panel) {
-        panel.setLayout(null); // Establecer el diseño del panel
+        panel.setLayout(new BorderLayout()); // Cambiar a BorderLayout
 
-        // Crear y configurar la etiqueta del usuario
+        // Crear un nuevo panel para la capa de color
+        RoundedPanel colorLayer = new RoundedPanel(new BorderLayout(), 250, new Color(80, 65, 169));
+        colorLayer.setPreferredSize(new Dimension(400, colorLayer.getHeight())); // Establecer el tamaño del panel
+        panel.add(colorLayer, BorderLayout.EAST); // Añadir el panel a la derecha
+        colorLayer.setLayout(null); // Establecer el diseño del panel
+
+        // Crear un nuevo panel para los componentes existentes
+        JPanel componentsPanel = new JPanel();
+        componentsPanel.setLayout(null); // Establecer el diseño del panel
+
+        // Crear y configurar las etiquetas
+        createAndConfigureLabels(colorLayer);
+
+        // Crear y configurar el botón de registro
+        createAndConfigureSignUpButton(colorLayer);
+
+        // Crear y configurar los componentes de inicio de sesión
+        createAndConfigureSignInComponents(componentsPanel);
+
+        componentsPanel.setBackground(new Color(238, 238, 238, 255)); // Establecer el color de fondo del panel
+
+        panel.add(componentsPanel, BorderLayout.CENTER); // Agregar el panel de componentes al centro
+    }
+
+    private void createAndConfigureLabels(RoundedPanel colorLayer) {
+        JLabel greetingLabel = new JLabel("WELCOME TO STEAM");
+        greetingLabel.setBounds(110, 170, 500, 25);
+        greetingLabel.setForeground(Color.white);
+        greetingLabel.setFont(new Font("Helvetica", Font.BOLD, 20));
+        colorLayer.add(greetingLabel);
+
+        JLabel invitationLabel = new JLabel("<html><div style='text-align: center;'>Register with your personal details<br>to get started</html>");
+        invitationLabel.setBounds(107, 200, 230, 50);
+        invitationLabel.setForeground(Color.white);
+        invitationLabel.setFont(new Font("Helvetica", Font.PLAIN, 14));
+        colorLayer.add(invitationLabel);
+    }
+
+    private void createAndConfigureSignUpButton(RoundedPanel colorLayer) {
+        JButton signUpButton = new JButton("SIGN UP");
+        signUpButton.setBounds(140, 260, 150, 35);
+        signUpButton.setOpaque(true); // Hacer el botón opaco
+        signUpButton.setBackground(new Color(80, 65, 165)); // Color de fondo
+        signUpButton.setForeground(Color.WHITE); // Color del texto
+
+        // Establece el borde del botón a una delgada línea blanca
+        Border whiteLineBorder = BorderFactory.createLineBorder(Color.WHITE);
+        signUpButton.setBorder(whiteLineBorder);
+
+        signUpButton.setContentAreaFilled(true); // Rellenar el área del contenido
+
+        colorLayer.add(signUpButton);
+    }
+
+
+    private void createAndConfigureSignInComponents(JPanel componentsPanel) {
         JLabel userLabel = new JLabel("SIGN IN WITH YOUR STEAM ACCOUNT");
         userLabel.setBounds(10, 20, 280, 25);
         userLabel.setForeground(Color.orange);
-        panel.add(userLabel);
+        componentsPanel.add(userLabel);
 
-        // Crear y configurar el campo de texto del usuario
         JTextField userText = new CustomTextField(20);
         userText.setBounds(7, 40, 300, 40);
-        panel.add(userText);
+        componentsPanel.add(userText);
 
-        // Crear y configurar la etiqueta de la contraseña
         JLabel passwordLabel = new JLabel("PASSWORD");
         passwordLabel.setBounds(10, 90, 80, 25);
         passwordLabel.setForeground(Color.DARK_GRAY);
-        panel.add(passwordLabel);
+        componentsPanel.add(passwordLabel);
 
-        // Crear y configurar el campo de texto de la contraseña
         JPasswordField passwordText = new CustomPasswordField(20);
         passwordText.setBounds(7, 110, 300, 40);
-        panel.add(passwordText);
+        componentsPanel.add(passwordText);
 
-        // Crear y configurar el botón de inicio de sesión
         JButton loginButton = new JButton("Sign in");
         loginButton.setBounds(50, 160, 200, 35);
-        loginButton.setBackground(Color.orange);
-        panel.add(loginButton);
+        componentsPanel.add(loginButton);
 
-        panel.setBackground(Color.gray); // Establecer el color de fondo del panel
-
-        // Añadir un oyente de acción al botón de inicio de sesión
         loginButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                // Intentar iniciar sesión con el nombre de usuario y la contraseña proporcionados
                 if (currentUser.login(userText.getText(), new String(passwordText.getPassword()))) {
                     JOptionPane.showMessageDialog(frame, "Login successful!"); // Mostrar un mensaje de éxito
                     displayMainMenu(); // Mostrar el menú principal
