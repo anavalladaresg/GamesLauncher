91b09d342da2d4002e4fe26afa4c1c0c7eb31227
Ana Valladares González <avalladaresgonzalez@danielcastelao.org>
2024-05-01 11:50:13 +0200

    Implementada muestra de la interfaz

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
+import com.launcher.UIController;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        new UIController();
     }
 }
