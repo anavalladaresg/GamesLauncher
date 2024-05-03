96e621d56f585e7e63aacf893ffd37cdca0f2e87
Ana Valladares González <avalladaresgonzalez@danielcastelao.org>
2024-05-03 12:35:18 +0200

    Atributos de Game implementados, con constructor, getters y setter

--- src/com/games/Game.java
+++ src/com/games/Game.java
@@ -1,4 +1,92 @@
 package com.games;
 
+import java.time.LocalDate;
+
 public class Game {
+    private int gameId;
+    private String gameName;
+    private String gameDescription;
+    private double gamePrice;
+    private String gameGenre;
+    private LocalDate releaseDate;
+    private String gameDeveloper;
+    private double gameRating;
+
+    public Game() {
+    }
+
+    public Game(int gameId, String gameName, String gameDescription, double gamePrice, String gameGenre, LocalDate releaseDate, String gameDeveloper, double gameRating) {
+        this.gameId = gameId;
+        this.gameName = gameName;
+        this.gameDescription = gameDescription;
+        this.gamePrice = gamePrice;
+        this.gameGenre = gameGenre;
+        this.releaseDate = releaseDate;
+        this.gameDeveloper = gameDeveloper;
+        this.gameRating = gameRating;
+    }
+
+    public int getGameId() {
+        return gameId;
+    }
+
+    public void setGameId(int gameId) {
+        this.gameId = gameId;
+    }
+
+    public String getGameName() {
+        return gameName;
+    }
+
+    public void setGameName(String gameName) {
+        this.gameName = gameName;
+    }
+
+    public String getGameDescription() {
+        return gameDescription;
+    }
+
+    public void setGameDescription(String gameDescription) {
+        this.gameDescription = gameDescription;
+    }
+
+    public double getGamePrice() {
+        return gamePrice;
+    }
+
+    public void setGamePrice(double gamePrice) {
+        this.gamePrice = gamePrice;
+    }
+
+    public String getGameGenre() {
+        return gameGenre;
+    }
+
+    public void setGameGenre(String gameGenre) {
+        this.gameGenre = gameGenre;
+    }
+
+    public LocalDate getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(LocalDate releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getGameDeveloper() {
+        return gameDeveloper;
+    }
+
+    public void setGameDeveloper(String gameDeveloper) {
+        this.gameDeveloper = gameDeveloper;
+    }
+
+    public double getGameRating() {
+        return gameRating;
+    }
+
+    public void setGameRating(double gameRating) {
+        this.gameRating = gameRating;
+    }
 }
