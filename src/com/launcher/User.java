5e647fa10d9ffadac7e8f2098c53098ed788275c
Ana <avalladaresgonzalez@danielcastelao.org>
2024-05-12 11:18:16 +0200

    # Conflicts: #	src/com/launcher/LibraryController.java #	src/com/launcher/SignInController.java #	src/com/launcher/SignUpController.java

--- src/com/launcher/User.java
+++ src/com/launcher/User.java
@@ -1,22 +1,30 @@
 package com.launcher;
 
 import com.games.Game;
+import database.DatabaseHandler;
 
 import java.util.ArrayList;
 
+/**
+ * This class represents a user in the system.
+ */
 public class User {
     private static int userIdCounter = 0;
-    /**
-     * List of games owned by the user
-     */
     ArrayList<Game> gamesOwned = new ArrayList<Game>();
-    /**
-     * List of users in the system
-     */
     ArrayList<User> users = new ArrayList<User>();
     private int userId;
     private String userName;
     private String password;
+
+    /**
+     * Constructor for the User class.
+     * It increments the userId each time a new user is created.
+     */
+    public User() {
+        userId = userIdCounter++;
+    }
+
+    // Getters and setters
 
     public static int getUserIdCounter() {
         return userIdCounter;
@@ -67,52 +75,18 @@ public class User {
     }
 
     /**
-     * Every time a new user is created, the userId is incremented by 1
-     */
-    public User() {
-        userId = userIdCounter++;
-    }
-
-    /**
-     * Sign up a new user
-     *
-     * @param userName username of the user
-     * @param password password of the user
-     * @return true if the user is signed up successfully, false otherwise
-     */
-    public boolean signUp(String userName, String password) {
-        for (User u : users) {
-            if (u.userName.equals(userName)) {
-                return false;
-            }
-        }
-        User newUser = new User();
-        newUser.userName = userName;
-        newUser.password = password;
-        users.add(newUser);
-        return true;
-    }
-
-    /**
-     * Login a user
-     *
-     * @param userName username of the user
-     * @param password password of the user
+     * This method logs in a user.
+     * @param userName the username
+     * @param password the password
      * @return true if the user is logged in successfully, false otherwise
      */
-
     public boolean login(String userName, String password) {
-        for (User u : users) {
-            if (u.userName.equals(userName) && (u.password.equals(password))) {
-                return true;
-            }
+        DatabaseHandler db = new DatabaseHandler();
+        db.connect();
+        if (db.userExists(userName)) {
+            String storedPassword = db.getPassword(userName);
+            return storedPassword != null && storedPassword.equals(password);
         }
         return false;
-    }
-
-    public void addGameToLibrary(Game game) {
-    }
-
-    public void removeGameFromLibrary(Game game) {
     }
 }
