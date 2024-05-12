78980c539d1d29c54e99a967ce72b86510038d89
Ana <avalladaresgonzalez@danielcastelao.org>
2024-05-12 20:03:15 +0200

    Getter & setter de arraylist de juegos

--- src/com/launcher/Library.java
+++ src/com/launcher/Library.java
@@ -7,4 +7,11 @@ import java.util.ArrayList;
 public class Library {
     ArrayList<Game> games = new ArrayList<Game>();
 
+    public ArrayList<Game> getGames() {
+        return games;
+    }
+
+    public void setGames(ArrayList<Game> games) {
+        this.games = games;
+    }
 }
