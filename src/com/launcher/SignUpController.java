286806cad6c21dca911e5669e4aea0cf89bc988b
Anx0Fdez <anxofernandezrodriguez05@gmail.com>
2024-06-01 21:05:57 +0200

    Implementado JavaDoc

--- src/com/launcher/SignUpController.java
+++ src/com/launcher/SignUpController.java
@@ -10,28 +10,35 @@ import javax.swing.border.CompoundBorder;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.awt.event.*;
+import java.util.Objects;
 
 /**
  * Representa un controlador para la pantalla de registro.
  */
 public class SignUpController {
-    private final JFrame frame; // Window frame
-    private final User currentUser = new User(); // Current user
+
+    /**
+     * La ventana.
+     */
+    private final JFrame frame;
+
+    /**
+     * El usuario actual.
+     */
+    private final User currentUser = new User();
 
     /**
      * Inicializa un nuevo SignUpController.
      */
     public SignUpController() {
-        frame = new JFrame("Xynx"); // Create a new frame with the title "Xynx"
-        frame.setSize(800, 500); // Set the size of the frame
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Set the close operation
-        frame.setResizable(false); // This prevents the user from resizing the window
-
-        JPanel panel = new JPanel(); // Create a new panel
-        frame.add(panel); // Add the panel to the frame
-        placeComponents(panel); // Place the components on the panel
-
-        frame.setVisible(true); // Make the frame visible
+        frame = new JFrame("Xynx");
+        frame.setSize(800, 500);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        JPanel panel = new JPanel();
+        frame.add(panel);
+        placeComponents(panel);
+        frame.setVisible(true);
     }
 
     /**
@@ -39,30 +46,18 @@ public class SignUpController {
      * @param panel El panel en el que se colocarán los componentes.
      */
     private void placeComponents(JPanel panel) {
-        panel.setLayout(new BorderLayout()); // Change to BorderLayout
-
-        // Create a new panel for the color layer
+        panel.setLayout(new BorderLayout());
         SignUpRoundedPanel colorLayer = new SignUpRoundedPanel(new BorderLayout(), 250, new Color(80, 65, 169));
-        colorLayer.setPreferredSize(new Dimension(400, colorLayer.getHeight())); // Set the size of the panel
-        panel.add(colorLayer, BorderLayout.WEST); // Add the panel to the right
-        colorLayer.setLayout(null); // Set the layout of the panel
-
-        // Create a new panel for the existing components
+        colorLayer.setPreferredSize(new Dimension(400, colorLayer.getHeight()));
+        panel.add(colorLayer, BorderLayout.WEST);
+        colorLayer.setLayout(null);
         JPanel componentsPanel = new JPanel();
-        componentsPanel.setLayout(null); // Set the layout of the panel
-
-        // Create and configure the labels
+        componentsPanel.setLayout(null);
         createAndConfigureLabels(colorLayer);
-
-        // Create and configure the sign up button
         createAndConfigureSignInButton(colorLayer);
-
-        // Create and configure the sign in components
         createAndConfigureSignInComponents(componentsPanel);
-
-        componentsPanel.setBackground(new Color(238, 238, 238, 255)); // Set the background color of the panel
-
-        panel.add(componentsPanel, BorderLayout.CENTER); // Add the components panel to the center
+        componentsPanel.setBackground(new Color(238, 238, 238, 255));
+        panel.add(componentsPanel, BorderLayout.CENTER);
     }
 
     /**
@@ -101,8 +96,32 @@ public class SignUpController {
      * @param colorLayer El panel en el que se colocará el botón.
      */
     private void createAndConfigureSignInButton(SignUpRoundedPanel colorLayer) {
+        JButton signUpButton = getSignUpButton();
+        signUpButton.setOpaque(false);
+        signUpButton.setBackground(new Color(80, 65, 165));
+        signUpButton.setFont(new Font("Helvetica", Font.BOLD, 14));
+        signUpButton.setForeground(Color.WHITE);
+        Border whiteLineBorder = new RoundedBorder(Color.WHITE, 10);
+        signUpButton.setBorder(whiteLineBorder);
+        signUpButton.setContentAreaFilled(true);
+        colorLayer.add(signUpButton);
+
+        signUpButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                displaySignInScreen();
+            }
+        });
+
+        configureListenerToSignUpButton(signUpButton);
+    }
+
+    /**
+     * Método para obtener el botón de registro.
+     * @return JButton
+     */
+    private static JButton getSignUpButton() {
         JButton signUpButton = new JButton("SIGN IN") {
-            // Override paintComponent to provide our own paint methods while preserving the original functionality
             protected void paintComponent(Graphics g) {
                 if (!isOpaque() && getBorder() instanceof RoundedBorder) {
                     Graphics2D g2 = (Graphics2D) g.create();
@@ -115,36 +134,24 @@ public class SignUpController {
             }
         };
         signUpButton.setBounds(105, 295, 150, 35);
-        signUpButton.setOpaque(false); // Make the button non-opaque
-        signUpButton.setBackground(new Color(80, 65, 165)); // Background color
-        signUpButton.setFont(new Font("Helvetica", Font.BOLD, 14));
-        signUpButton.setForeground(Color.WHITE); // Text color
+        return signUpButton;
+    }
 
-        // Set the border of the button to a rounded border with a thin white line
-        Border whiteLineBorder = new RoundedBorder(Color.WHITE, 10); // 10 is the radius of the border
-        signUpButton.setBorder(whiteLineBorder);
-
-        signUpButton.setContentAreaFilled(true); // Fill the content area
-
-        colorLayer.add(signUpButton);
-
-        signUpButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                displaySignInScreen();
-            }
-        });
-
+    /**
+     * Método para configurar el listener del botón de registro.
+     * @param signUpButton El botón de registro.
+     */
+    private void configureListenerToSignUpButton(JButton signUpButton) {
         signUpButton.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseEntered(MouseEvent e) {
-                signUpButton.setBackground(new Color(60, 45, 145)); // Color más oscuro cuando el ratón entra
+                signUpButton.setBackground(new Color(60, 45, 145));
                 signUpButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
             }
 
             @Override
             public void mouseExited(MouseEvent e) {
-                signUpButton.setBackground(new Color(80, 65, 165)); // Color original cuando el ratón sale
+                signUpButton.setBackground(new Color(80, 65, 165));
                 signUpButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
             }
         });
@@ -156,7 +163,6 @@ public class SignUpController {
      */
     private void createAndConfigureSignInComponents(JPanel componentsPanel) {
 
-        // Centra la ventana en la pantalla
         frame.setLocationRelativeTo(null);
 
         JLabel signInLabel = new JLabel("SIGN UP");
@@ -193,35 +199,52 @@ public class SignUpController {
         passwordText.setBorder(new CompoundBorder(new RoundedBorder(new Color(114, 114, 114), 10), new EmptyBorder(0, 10, 0, 0)));
         componentsPanel.add(passwordText);
 
-        // Sign in button
-        JButton signUpButton = new JButton("SIGN UP") {
-            @Override
-            protected void paintComponent(Graphics g) {
-                if (!isOpaque() && getBorder() instanceof RoundedBorder) {
-                    Graphics2D g2 = (Graphics2D) g.create();
-                    g2.setPaint(getBackground());
-                    g2.fill(((RoundedBorder) getBorder()).getBorderShape(0, 0, getWidth() - 1, getHeight() - 1));
-                    g2.dispose();
-                }
-                super.paintComponent(g);
-            }
-        };
-        signUpButton.setBounds(125, 320, 150, 35); // Set the bounds of the button
-        signUpButton.setOpaque(false); // Make the button non-opaque
-        signUpButton.setBackground(new Color(80, 65, 165)); // Set the background color
-        signUpButton.setFont(new Font("Helvetica", Font.BOLD, 14)); // Set the font
-        signUpButton.setForeground(Color.WHITE);
-        signUpButton.setBorder(new RoundedBorder(Color.WHITE, 10)); // 10 is the radius of the border
-        signUpButton.setContentAreaFilled(true);
+        JButton signUpButton = getUpButton();
         componentsPanel.add(signUpButton);
 
-        ImageIcon imageIcon = new ImageIcon(getClass().getResource("/com/images/XynxLogoVentana.png"));
-        Image image = imageIcon.getImage(); // Transforma el ImageIcon en Image
-        frame.setIconImage(image); // Establece la imagen como icono de la ventana
+        ImageIcon imageIcon = new ImageIcon(Objects.requireNonNull(getClass().getResource("/com/images/XynxLogoVentana.png")));
+        Image image = imageIcon.getImage();
+        frame.setIconImage(image);
 
-        /**
-         * This method listens for the enter key to be pressed and triggers the singIn button.
-         */
+
+        configureKeyListener(userText, passwordText, signUpButton);
+
+        signUpButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String username = userText.getText();
+                String password = new String(passwordText.getPassword());
+
+                String regex = "^[a-zA-Z0-9]+$"; //Solo permite caracteres alfanuméricos
+
+                if (!username.matches(regex) || !password.matches(regex)) {
+                    JOptionPane.showMessageDialog(frame,
+                            "<html><font color='red'>Username and password can only contain alphanumeric characters.</font></html>",
+                            "Error",
+                            JOptionPane.ERROR_MESSAGE);
+                    return;
+                }
+
+                currentUser.setUserName(username);
+                currentUser.setPassword(password);
+
+                UserDatabaseHandler udb = new UserDatabaseHandler();
+                if (udb.userExists(currentUser.getUserName())) {
+                    JOptionPane.showMessageDialog(frame,
+                            "<html><font color='red'>User already exists. Please choose a different username.</font></html>",
+                            "Error",
+                            JOptionPane.ERROR_MESSAGE);
+                } else {
+                    udb.addUser(currentUser.getUserName(), currentUser.getPassword());
+                    JOptionPane.showMessageDialog(frame, "Registration successful!");
+                    displayMainMenu();
+                }
+            }
+        });
+        configureListenerToSignUpButton(signUpButton);
+    }
+
+    static void configureKeyListener(PlaceholderTextField userText, PlaceholderPasswordField passwordText, JButton signUpButton) {
         userText.addKeyListener(new KeyAdapter() {
             public void keyPressed(KeyEvent e) {
                 if (e.getKeyCode() == KeyEvent.VK_ENTER) {
@@ -237,55 +260,42 @@ public class SignUpController {
                 }
             }
         });
+    }
 
-        // Add a listener to the sign in button
-        signUpButton.addActionListener(new ActionListener() {
+    /**
+     * Método para obtener el botón de registro.
+     * @return JButton
+     */
+    private static JButton getUpButton() {
+        JButton signUpButton = getButton();
+        signUpButton.setOpaque(false); // Make the button non-opaque
+        signUpButton.setBackground(new Color(80, 65, 165));
+        signUpButton.setFont(new Font("Helvetica", Font.BOLD, 14));
+        signUpButton.setForeground(Color.WHITE);
+        signUpButton.setBorder(new RoundedBorder(Color.WHITE, 10));
+        signUpButton.setContentAreaFilled(true);
+        return signUpButton;
+    }
+
+    /**
+     * Método para obtener un botón.
+     * @return JButton
+     */
+    private static JButton getButton() {
+        JButton signUpButton = new JButton("SIGN UP") {
             @Override
-            public void actionPerformed(ActionEvent e) {
-                String username = userText.getText();
-                String password = new String(passwordText.getPassword());
-
-                String regex = "^[a-zA-Z0-9]+$"; // Solo permite caracteres alfanuméricos
-
-                if (!username.matches(regex) || !password.matches(regex)) {
-                    JOptionPane.showMessageDialog(frame,
-                            "<html><font color='red'>Username and password can only contain alphanumeric characters.</font></html>",
-                            "Error",
-                            JOptionPane.ERROR_MESSAGE);
-                    return;
+            protected void paintComponent(Graphics g) {
+                if (!isOpaque() && getBorder() instanceof RoundedBorder) {
+                    Graphics2D g2 = (Graphics2D) g.create();
+                    g2.setPaint(getBackground());
+                    g2.fill(((RoundedBorder) getBorder()).getBorderShape(0, 0, getWidth() - 1, getHeight() - 1));
+                    g2.dispose();
                 }
-
-                currentUser.setUserName(username);
-                currentUser.setPassword(password);
-
-                UserDatabaseHandler udb = new UserDatabaseHandler();
-                if (udb.userExists(currentUser.getUserName())) {
-                    // User already exists, show an error message
-                    JOptionPane.showMessageDialog(frame,
-                            "<html><font color='red'>User already exists. Please choose a different username.</font></html>",
-                            "Error",
-                            JOptionPane.ERROR_MESSAGE);
-                } else {
-                    // User does not exist, proceed with registration
-                    udb.addUser(currentUser.getUserName(), currentUser.getPassword());
-                    JOptionPane.showMessageDialog(frame, "Registration successful!");
-                    displayMainMenu();
-                }
+                super.paintComponent(g);
             }
-        });
-        signUpButton.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseEntered(MouseEvent e) {
-                signUpButton.setBackground(new Color(60, 45, 145)); // Color más oscuro cuando el ratón entra
-                signUpButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
-            }
-
-            @Override
-            public void mouseExited(MouseEvent e) {
-                signUpButton.setBackground(new Color(80, 65, 165)); // Color original cuando el ratón sale
-                signUpButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
-            }
-        });
+        };
+        signUpButton.setBounds(125, 320, 150, 35);
+        return signUpButton;
     }
 
     /**
@@ -294,8 +304,8 @@ public class SignUpController {
     public void displaySignInScreen() {
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
-                frame.dispose(); // Close the current frame
-                new SignInController(); // Create a new instance of UIController
+                frame.dispose();
+                new SignInController();
             }
         });
     }
@@ -306,8 +316,8 @@ public class SignUpController {
     public void displayMainMenu() {
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
-                frame.dispose(); // Close the current frame
-                new LibraryController(currentUser.getUserName()); // Create a new instance of MainMenuController
+                frame.dispose();
+                new LibraryController(currentUser.getUserName());
             }
         });
     }
