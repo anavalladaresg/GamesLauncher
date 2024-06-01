79213ccc94e0f4b00438bba519ef6afe13deba89
Anx0Fdez <anxofernandezrodriguez05@gmail.com>
2024-06-01 21:06:00 +0200

    Implementado JavaDoc

--- src/com/launcher/SignUpRoundedPanel.java
+++ src/com/launcher/SignUpRoundedPanel.java
@@ -35,13 +35,11 @@ class SignUpRoundedPanel extends JPanel {
         Graphics2D graphics = (Graphics2D) g;
         graphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
-        // Draws the rounded panel with borders.
         graphics.setColor(backgroundColor);
         graphics.fillRoundRect(0, 0, width - 1, height - 1, cornerRadius, cornerRadius); //paint background
         graphics.setColor(getForeground());
         graphics.drawRoundRect(0, 0, width - 1, height - 1, cornerRadius, cornerRadius); //paint border
 
-        // Draws the right corners as square.
         graphics.setColor(backgroundColor); // Use the same background color
         graphics.fillRect(0, 0, width / 2, cornerRadius);
         graphics.fillRect(0, height - cornerRadius, width / 2, cornerRadius);
