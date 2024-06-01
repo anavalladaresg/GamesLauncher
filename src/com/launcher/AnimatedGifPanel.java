349111cf4a5d50e453786bf66584b02c0131387e
Anx0Fdez <anxofernandezrodriguez05@gmail.com>
2024-06-01 19:59:11 +0200

    Javadoc implementado

--- src/com/launcher/AnimatedGifPanel.java
+++ src/com/launcher/AnimatedGifPanel.java
@@ -5,10 +5,19 @@ import java.awt.*;
 import java.io.File;
 import java.io.IOException;
 
+/**
+ * Clase que representa un panel con una imagen GIF animada.
+ */
 public class AnimatedGifPanel extends JPanel {
 
+    /**
+     * Imagen GIF animada
+     */
     private ImageIcon gifImage;
 
+    /**
+     * Constructor por defecto.
+     */
     public AnimatedGifPanel() {
         try {
             // Cargar la imagen GIF como ImageIcon
@@ -18,6 +27,10 @@ public class AnimatedGifPanel extends JPanel {
         }
     }
 
+    /**
+     * Protege el <code>Graphics</code> del componente.
+     * @param g the <code>Graphics</code> object to protect
+     */
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
@@ -27,21 +40,12 @@ public class AnimatedGifPanel extends JPanel {
         }
     }
 
+    /**
+     * Devuelve el tamaño preferido del panel.
+     * @return Dimension
+     */
     @Override
     public Dimension getPreferredSize() {
         return new Dimension(gifImage.getIconWidth(), gifImage.getIconHeight());
-    }
-
-    public static void main(String[] args) {
-        JFrame frame = new JFrame("Animated GIF Panel");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLayout(new BorderLayout());
-
-        AnimatedGifPanel animatedGifPanel = new AnimatedGifPanel();
-        frame.add(animatedGifPanel, BorderLayout.CENTER);
-
-        frame.pack();
-        frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
     }
 }
